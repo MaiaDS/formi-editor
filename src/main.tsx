ab60d1ac503e638e1e4faa29c826a335ffa3f3e6
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-11-11 14:51:52 -0500

    fix imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.scss'
-import Formi from './components/editor/index.tsx'
+import Formi from './components/form-generator/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
