04dad8b93567e68d0b2ce58de4b774975c217fa2
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-11-11 14:49:56 -0500

    fix placeholder

---
+++ src/components/form-generator/editor/placeholder/index.tsx
@@ -0,0 +1,11 @@
+import styles from './style.module.scss'
+
+const Placeholder = () => {
+    return (
+        <span className={styles.placeholder}>
+            Drag and Drop components here
+        </span>
+    )
+}
+
+export default Placeholder
