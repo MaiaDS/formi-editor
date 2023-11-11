4532f43a4cfce3c98e7c85f2be174d3d0f2722d3
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2023-11-11 14:48:09 -0500

    fix form component

---
+++ src/components/form-generator/editor/form-preview/form-component/index.tsx
@@ -0,0 +1,43 @@
+import { FC, useMemo } from 'react'
+import { FormComponent } from '@/types'
+import ComponentFactory from '@/components/component-factory'
+import styles from './style.module.scss'
+
+interface Props {
+    data: FormComponent
+    selectedComponent: string | undefined
+    selectComponent: (component: string) => void
+    removeComponent: (componentId: string) => void
+}
+
+const EditorComponent: FC<Props> = ({
+    data,
+    selectedComponent,
+    selectComponent,
+    removeComponent,
+}) => {
+    const isActive: boolean = useMemo(() => {
+        if (selectedComponent) {
+            selectedComponent.includes(data.id)
+        }
+        return false
+    }, [selectedComponent])
+
+    return (
+        <li className={`${styles.component} ${isActive ? styles.active : ''}`}>
+            <button
+                type="button"
+                onClick={() => selectComponent(data.id)}
+                key={data.id}>
+                <ComponentFactory component={data} />
+            </button>
+            {selectedComponent?.includes(data.id) && (
+                <button type="button" onClick={() => removeComponent(data.id)}>
+                    Delete
+                </button>
+            )}
+        </li>
+    )
+}
+
+export default EditorComponent
